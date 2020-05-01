c4f73287128f8e6993b07ebf2d8c3394325423d1
GLEDSONS831 <glegogle84@gmail.com>
2020-05-01 19:16:54 -0300

    :pencil: add functions

---
+++ src/database/migrations/20200430203554_create_table_users.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', function(table){
+        
+      table.increments('id');
+      table.text('username').unique().notNullable();
+  
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('update_at').defaultTo(knex.fn.now());
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+  };
+  
