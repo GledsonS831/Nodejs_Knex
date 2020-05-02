b758882a3c4e7bf31553bbb9d73eab1c191d8543
GLEDSONS831 <glegogle84@gmail.com>
2020-05-01 21:06:29 -0300

    :pencil: add routes

--- src/server.js
+++ src/server.js
@@ -3,7 +3,25 @@ const routes = require('./routes');
 
 const app = express();
 
+//configurar para que o express utilize o json
+app.use(express.json());
+
 app.use(routes);
+
+//not found
+app.use((req, res, next) =>{
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+//cactch all
+app.use( (error, req, res, next) =>{
+    res.status(error.status || 500)
+    res.json({
+        error: error.message
+    })
+})
 
 app.listen(3333, () =>{
     console.log("running");
